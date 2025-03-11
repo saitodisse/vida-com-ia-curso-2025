82615695b69d41a28ac41ac5a32fd67ccd8f97c4
Julio MS <saitodisse@gmail.com>
2025-03-11 15:13:58 -0300

    Perceptrons

---
+++ 03-mlp.py
@@ -0,0 +1,27 @@
+# O que são Perceptrons?
+# Perceptrons são modelos de redes neurais artificiais que são capazes de aprender a partir de dados de entrada e saída.
+# Eles são compostos por uma camada de entrada, uma camada oculta e uma camada de saída.
+# A camada de entrada é onde os dados de entrada são inseridos.
+# A camada oculta é onde os dados são processados e transformados.
+# A camada de saída é onde os dados de saída são produzidos.
+
+# Sua formula é:
+# y = f(w1*x1 + w2*x2 + ... + wn*xn + b)
+# onde:
+# y é a saída
+# f é a função de ativação
+# w1, w2, ..., wn são os pesos
+# x1, x2, ..., xn são os dados de entrada
+# b é o bias
+
+# A função de ativação mais comum é a função sigmoide:
+# f(x) = 1 / (1 + e^(-x))
+
+# A função sigmoide é uma função que transforma qualquer valor em um valor entre 0 e 1.
+# Ela é uma função contínua e diferenciável, o que é importante para o treinamento das redes neurais.
+
+# https://notebooklm.google.com/notebook/822badd3-19de-42c3-a520-e0b5788bc38c?_gl=1*1bgry1v*_ga*MjEzMzU3MzI4OC4xNzM4MTg2NTE3*_ga_W0LDH41ZCB*MTc0MTcxMDQ0Ni4xMC4wLjE3NDE3MTA0NDYuNjAuMC4w
+# reff: https://chatgpt.com/c/67d06007-ed74-8007-854a-f8ba2f25947e
+# reff: https://gemini.google.com/app/b8ff1cc0ac6cbd22
+# reff: https://grok.com/chat/48106f98-93a1-499c-b828-70004328adfd
+
