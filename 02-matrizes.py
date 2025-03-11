c99063c4acd47abf59b60f68c5fdc4257d87ccd9
Julio MS <saitodisse@gmail.com>
2025-03-11 10:11:27 -0300

    Add matrix multiplication examples with NumPy and PyTorch, including error handling

--- 02-matrizes.py
+++ 02-matrizes.py
@@ -28,4 +28,49 @@ print("\nResultado da multiplicação usando o Numpy:")
 print(D)
 
 
+# tentando multiplicar matrizes com tamanhos inválidos
+A = np.array([[1, 2, 20], [3, 4, 50], [5, 6, 60]])
+B = np.array([[7, 8], [9, 10]])
+
+try:
+    C = A @ B
+    print("\nResultado da multiplicação:")
+    print(C)
+except ValueError as e:
+    print(f"Erro: {e}")
+
+print("\n\n")
+#  ----------------------
+
+# verificar a minha versão do cuda: PS> nvcc --version
+# instalar o pytorch daqui: https://pytorch.org/get-started/locally/
+# PS> pip3 install torch torchvision torchaudio --index-url https://download.pytorch.org/whl/cu124
+
+import torch
+print("torch.cuda.is_available()? {}".format(torch.cuda.is_available()))
+print("torch.version.cuda? {}".format(torch.version.cuda))
+print("torch.__version__? {}".format(torch.__version__))
+
+# Converter as matrizes para tensores do Pytorch
+A = np.array([[1, 2], [3, 4], [5, 6]])
+B = np.array([[7, 8], [9, 10]])
+
+A_tensor = torch.tensor(A)
+B_tensor = torch.tensor(B)
+
+# Usando o operador @
+C_tensor = A_tensor @ B_tensor
+print("\nResultado da multiplicação usando o operador @:")
+print(C_tensor)
+
+# Usando a função do Pytorch
+D_tensor = torch.matmul(A_tensor, B_tensor)
+print("\nResultado da multiplicação usando a função do Pytorch:")
+print(D_tensor)
+
+
+
+
+
+
 
